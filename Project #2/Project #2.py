985af7637c0f9c3923202bc1ac27e7a50509d186
DrewPickett <drewtpickett@gmail.com>
2025-04-15 16:15:42 -0700

    Code Update

--- Project #2/Project #2.py
+++ Project #2/Project #2.py
@@ -1,7 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
+import asyncio
+import multiprocessing
+import threading
+import time
 
-# In[ ]:
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
 
 
 
