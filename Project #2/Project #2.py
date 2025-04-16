20bb0703735b00044c5c440c98bb7022dfa28c27
DrewPickett <drewtpickett@gmail.com>
2025-04-15 20:51:48 -0700

    async update

--- Project #2/Project #2.py
+++ Project #2/Project #2.py
@@ -11,6 +11,23 @@ def is_prime(n):
             return False
     return True
 
+def highest_prime_calculation():
+    highest_prime = 0
+    n = 0
+    start_time = time.time()
+    while time.time() - start_time() <= 180:
+        if is_prime(n):
+            highest_prime = n
+        n += 1
+    return highest_prime
 
+async def asynchronous_calc():
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(None,highest_prime_calculation)
 
+async def main():
+    async_prime = await asynchronous_calc()
+    print(f"Async Highest Prime: {async_prime}")
 
+if __name__ == "__main__":
+    asyncio.run(main())
