7167b9c4b757be6e09f2b1330a90e5185e0a6be1
DrewPickett <drewtpickett@gmail.com>
2025-05-17 21:27:34 -0700

    added file and contents

---
+++ Homework/Homework3/data_classes.py
@@ -0,0 +1,31 @@
+import json
+
+class Person:
+    def __init__(self, name, age, email):
+        self.name = name
+        self.age = age
+        self.email = email
+
+class Student(Person):
+    def __init__(self, name, age, email, student_id):
+        super().__init__(name, age, email)
+        self.student_id = student_id
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "age": self.age,
+            "email": self.email,
+            "student_id": self.student_id
+        }
+
+    def save(self):
+        file = f"{self.name}_Info.json"
+        with open(file, "w") as f:
+            json.dump(self.to_dict(), f, indent=4)
+
+    def display(self):
+        file = f"{self.name}_Info.json"
+        with open(file, "r") as f:
+            info = json.load(f)
+            print(json.dumps(info, indent=4))
